16c4fcbec78154651489029e4a55b57a0cb7707f
aismeg <0>
2024-02-01 16:24:42 +0300

    nav and accordion done

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -3,13 +3,21 @@ const nav = () => {
     const section = document.querySelectorAll('section');
 
     const activeToggle = e => {
+        const regex = /^[^#]*#/g;
+
         e.preventDefault();
+
+        section.forEach(tab => {
+            tab.id === e.target.href.replace(regex, '') ?
+                tab.style.display = 'block' :
+                tab.style.display = 'none';
+        })
 
         nav.forEach(e => {
             e.classList.remove('actual-page');
         })
 
-        e.target.classList.add('actual-page')
+        e.target.classList.add('actual-page');
     }
 
     nav.forEach(e => {
